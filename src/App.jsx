7871995e9d591a29657dc2d19e6c05be58808c0f
Manish Kumar <manish.login01@gmail.com>
2024-08-25 23:06:35 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <h1>Demo</h1>
+      <p>Nothing else...</p>
+    </>
+  );
+};
+
+export default App;
