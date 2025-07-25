27e4281015984aeaf90d9e1a587bae8a823909af
Manish Kumar <manish.login01@gmail.com>
2025-07-25 23:29:50 +0530

    bug : remove unnecessary icons and fix toast import

--- client/src/pages/admin/ListBlog.jsx
+++ client/src/pages/admin/ListBlog.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { toast } from "react-toastify";
+import toast from "react-hot-toast";
 
 const ListBlog = () => {
   const [blogs, setBlogs] = useState([]);
