1404780ea28748ecf373e2d8279fe10fee58a9c7
Manish Kumar <manish.login01@gmail.com>
2025-08-09 14:31:13 +0530

    fix : adjust cookie SameSite setting for local and production environments

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -8,7 +8,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const COOKIE_OPTIONS = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "None",
+  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
